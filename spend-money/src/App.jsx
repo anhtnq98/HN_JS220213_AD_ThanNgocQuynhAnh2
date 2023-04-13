141d76585b4d059bcc66422333145275be55ac92
anhtnq98 <kethanlinh.otaku@gmail.com>
2023-04-13 17:44:21 +0700

    add

--- spend-money/src/App.jsx
+++ spend-money/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div class="to-spend">
         <p>
-          To Spend <strong>$1500000000000</strong>
+          To Spend <strong>$128,000,000,000</strong>
         </p>
         <p>You have a lot of money</p>
       </div>
